2dfa8b6488ad94e1e22a717b019fd5437a3c9a66
Andrew <lonewolf575309@gmail.com>
2024-09-15 16:21:39 -0700

    Added expanded parent child for units/tiles
    Was needed for handling selectables

--- War Room/Assets/Scripts/Classes/LandTile.cs
+++ War Room/Assets/Scripts/Classes/LandTile.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class LandTile : Tile
 {
+    public struct landResources
+    {
+        int currentOil;
+        int totalOil;
+        int currentIron;
+        int totalIron;
+        int currentOther;
+        int totalOther;
+        int stressWorth;
+        int factoryCount;
+    }
+
 
 }
