4ab69d03f90ae92241aa20c9160e7d79f09816cb
Andrew <lonewolf575309@gmail.com>
2024-09-15 14:48:37 -0700

    Added Ships

--- War Room/Assets/Scripts/Classes/Tile.cs
+++ War Room/Assets/Scripts/Classes/Tile.cs
@@ -6,5 +6,57 @@ public class Tile : MonoBehaviour
 {
     public string tileName;
     public List<Tile> adjacentTiles;
-    
+    public bool selected = false;
+
+    private void Start()
+    {
+        foreach (Tile tile in adjacentTiles)
+        {
+            if (tile == this)
+            {
+                Debug.Log("Warning: tile:" + this.name + " is contained within itself.");
+                Material mat = getMat(this.gameObject, "Outline");
+                mat.SetColor("_BaseColor", Color.yellow);
+            }
+            else if (!tile.adjacentTiles.Contains(this))
+            {
+                Debug.Log("Warning: tile:" + tile.name + " does not contain tile " + this.name);
+                Material mat = getMat(tile.gameObject, "Outline");
+                mat.SetColor("_BaseColor", Color.red);
+            }
+        }
+    }
+
+    private Material getMat(GameObject tile, string name)
+    {
+        MeshRenderer mr = tile.GetComponent<MeshRenderer>();
+        Material mat = null;
+        if (mr != null)
+        {
+            foreach (Material material in mr.materials)
+            {
+                if (material.name.Contains(name))
+                {
+                    mat = material;
+                }
+            }
+        }
+        return mat;
+    }
+
+    public IEnumerator visualSelect()
+    {
+        
+        selected = true;
+        float tick = 0f;
+        Material mat = getMat(this.gameObject, "Outline");
+        Color originalColor = mat.color;
+        while (selected)
+        {
+            tick += Time.deltaTime * 1;
+            mat.color = Color.Lerp(originalColor, Color.black, Mathf.PingPong(Time.time, 1));
+            yield return null;
+        }
+        mat.color = originalColor;
+    }
 }
---
+++ War Room/Assets/Scripts/InteractionHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+    // Update is called once per frame
+    List<Tile> selectedTiles;
+    Tile selectedTile;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Tile clickedTile = hit.transform.gameObject.GetComponent<Tile>();
+                if (clickedTile != null)
+                {
+                    deselectTile(selectedTile);
+                    selectedTile = null;
+                    IEnumerator coroutine = clickedTile.visualSelect();
+                    StartCoroutine(coroutine);
+                    selectedTile = clickedTile;
+                    //selectedTiles.Add(clickedTile);
+                }
+                else
+                {
+                    deselectTile(selectedTile);
+                    selectedTile = null;
+                }
+            }
+            else
+            {
+                deselectTile(selectedTile);
+                selectedTile = null;
+            }
+        }
+    }
+
+    private void deselectTiles(List<Tile> thing)
+    {
+        foreach (Tile tile in selectedTiles)
+        {
+            tile.selected = false;
+        }
+    }
+
+    private void deselectTile(Tile tile)
+    {
+        if (tile != null)
+        {
+            tile.selected = false;
+        }
+    }
+
+    private List<Tile> pingRadious(int rad, List<Tile> tiles)
+    {
+        foreach (Tile tile in tiles)
+        {
+
+        }
+        return tiles;
+    } 
+}
